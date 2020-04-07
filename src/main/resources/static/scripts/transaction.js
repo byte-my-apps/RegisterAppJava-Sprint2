057ab2b8df8ecda3438b2ed8504707dc11b441a2
Johnny Doan <55829768+johnnydoan850@users.noreply.github.com>
2020-04-07 09:37:58 -0500

    JS Updates
    provided basic functions for elements that we know are going to be on the Transaction and TransactionSummary pages

--- src/main/resources/static/scripts/transaction.js
+++ src/main/resources/static/scripts/transaction.js
@@ -1,2 +1,27 @@
-// There is currently no JS implementation of the code.
-// It is non-functional as we are making sure the design is up to the group's plans.
+document.addEventListener("DOMContentLoaded", function(event) {
+	getSearchActionElement().addEventListener(
+		"click",
+		() => {});
+
+	getMainMenuActionElement().addEventListener(
+		"click",
+		() => { window.location.assign("/mainMenu"); });
+
+	getTransactionSummActionElement().addEventListener(
+		"click",
+		() => { window.location.assign("/transactionSummary"); });
+});
+
+// Getters and setters
+function getSearchActionElement() {
+    return document.getElementById("searchButton");
+}
+
+function getMainMenuActionElement() {
+	return document.getElementById("mainMenuButton");
+}
+
+function getTransactionSummActionElement() {
+    return document.getElementById("transactionSumButton");
+}
+// End getters and setters
