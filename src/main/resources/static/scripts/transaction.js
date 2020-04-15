e053d93387de8bed6a17966f1bbd52c1575ec947
Jack Gaither <jack.a.gaither@gmail.com>
2020-04-14 19:09:40 -0500

    Added visual representation of item removal
    Added Remove buttons and connected them to the transaction.js file on top of Josh's existing html code. Added event listeners for the remove buttons in the transaction.js file so that they visually get removed as an element. Further along this will be the style how the user ends up calling removing an actual cart item. The transaction.js changes were made on top of Johnny's original file and format.

--- src/main/resources/static/scripts/transaction.js
+++ src/main/resources/static/scripts/transaction.js
@@ -10,6 +10,43 @@ document.addEventListener("DOMContentLoaded", function(event) {
 	getTransactionSummActionElement().addEventListener(
 		"click",
 		() => { window.location.assign("/transactionSummary"); });
+
+	getItemElement1().addEventListener(
+                "click",
+                () => {removeItem1();});
+
+        getItemElement2().addEventListener(
+                "click",
+                () => {removeItem2();});
+
+        getItemElement3().addEventListener(
+                "click",
+                () => {removeItem3();});
+
+        getItemElement4().addEventListener(
+                "click",
+                () => {removeItem4();});
+
+        getItemElement5().addEventListener(
+                "click",
+                () => {removeItem5();});
+
+        getItemElement6().addEventListener(
+                "click",
+                () => {removeItem6();});
+
+        getItemElement7().addEventListener(
+                "click",
+                () => {removeItem7();});
+
+        getItemElement8().addEventListener(
+                "click",
+                () => {removeItem8();});
+
+        getItemElement9().addEventListener(
+                "click",
+                () => {removeItem9();});
+
 });
 
 // Getters and setters
@@ -24,4 +61,88 @@ function getMainMenuActionElement() {
 function getTransactionSummActionElement() {
     return document.getElementById("transactionSumButton");
 }
+
+function getItemElement1()
+{
+    return document.getElementById("item1");
+}
+
+function getItemElement2()
+{
+    return document.getElementById("item2");
+}
+
+function getItemElement3()
+{
+    return document.getElementById("item3");
+}
+
+function getItemElement4()
+{
+    return document.getElementById("item4");
+}
+
+function getItemElement5()
+{
+    return document.getElementById("item5");
+}
+
+function getItemElement6()
+{
+    return document.getElementById("item6");
+}
+
+function getItemElement7()
+{
+    return document.getElementById("item7");
+}
+
+function getItemElement8()
+{
+    return document.getElementById("item8");
+}
+
+function getItemElement9()
+{
+    return document.getElementById("item9");
+}
+
+function removeItem1() {
+    document.getElementById("item1").innerHTML = "";
+}
+function removeItem2() {
+    document.getElementById("item2").innerHTML = "";
+}
+function removeItem3() {
+    document.getElementById("item3").innerHTML = "";
+}
+function removeItem4() {
+    document.getElementById("item4").innerHTML = "";
+}
+function removeItem5() {
+    document.getElementById("item5").innerHTML = "";
+}
+function removeItem6() {
+    document.getElementById("item6").innerHTML = "";
+}
+function removeItem7() {
+    document.getElementById("item7").innerHTML = "";
+}
+function removeItem8() {
+    document.getElementById("item8").innerHTML = "";
+}
+function removeItem9() {
+    document.getElementById("item9").innerHTML = "";
+}
 // End getters and setters
+
+
+
+
+
+
+
+
+
+
+
