9540ce3a9051900717e482cd135a60b13b92cec3
Johnny Doan <55829768+johnnydoan850@users.noreply.github.com>
2020-04-14 20:59:22 -0500

    Update transactionSummary.js
    not completed yet, added functionality for calculating total price and quantity

--- src/main/resources/static/scripts/transactionSummary.js
+++ src/main/resources/static/scripts/transactionSummary.js
@@ -1,12 +1,32 @@
 document.addEventListener("DOMContentLoaded", function(event) {
 	getBackActionElement().addEventListener(
 		"click",
-		() => { window.location.assign("/mainMenu"); });
+		() => { window.location.assign("/transaction"); });
 
 	getSubmitTransActionElement().addEventListener(
 		"click",
 		() => { });
 });
+
+function calculatePrice() {
+	var table = document.getElementById("TransactionSummary");
+	var totalPrice = 0;
+
+	for(var i = 1; i < table.rows.length - 1; i++) {
+		totalPrice += parseInt(table.rows[i].cells[2].innerHTML);
+	}
+	return totalPrice;
+}
+
+function calculateQuantity() {
+	var table = document.getElementById("TransactionSummary");
+	var totalQuantity = 0;
+
+	for(var i = 1; i < table.rows.length - 1; i++) {
+		totalQuantity += parseInt(table.rows[i].cells[1].innerHTML);
+	}
+	return totalQuantity;
+}
 
 // Getters and setters
 function getBackActionElement() {
