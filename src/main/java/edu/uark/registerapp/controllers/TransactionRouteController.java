5be6842487e905795b43b57c5b0fa346b74612a7
Johnny Doan <55829768+johnnydoan850@users.noreply.github.com>
2020-04-22 19:00:22 -0500

    Update TransactionRouteController.java
    now Transaction.html is accessible; not completely finished(?)

--- src/main/java/edu/uark/registerapp/controllers/TransactionRouteController.java
+++ src/main/java/edu/uark/registerapp/controllers/TransactionRouteController.java
@@ -1,4 +1,52 @@
 package edu.uark.registerapp.controllers;
 
-public class TransactionRouteController {
+import java.util.Map;
+import java.util.Optional;
+import java.util.UUID;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import edu.uark.registerapp.commands.transactions.TransactionCreateCommand;
+import edu.uark.registerapp.models.entities.ActiveUserEntity;
+import edu.uark.registerapp.controllers.enums.ViewModelNames;
+import edu.uark.registerapp.controllers.enums.ViewNames;
+
+@Controller
+@RequestMapping(value = "/transaction")
+public class TransactionRouteController extends BaseRouteController {
+    @RequestMapping(method = RequestMethod.GET)
+    public ModelAndView showTransaction(
+		@RequestParam final Map<String, String> queryParameters,
+		final HttpServletRequest request
+	) {
+        final Optional<ActiveUserEntity> activeUserEntity =
+			this.getCurrentUser(request);
+		if (!activeUserEntity.isPresent()) {
+			return this.buildInvalidSessionResponse();
+        } 
+        
+        ModelAndView modelAndView =
+			this.setErrorMessageFromQueryString(
+				new ModelAndView(ViewNames.TRANSACTION.getViewName()),
+                queryParameters);
+        
+        modelAndView.addObject(
+			ViewModelNames.IS_ELEVATED_USER.getValue(),
+            this.isElevatedUser(activeUserEntity.get()));
+                
+        return modelAndView;
+    }
+
+    // Properties
+	@Autowired
+	private TransactionCreateCommand transactionCreateCommand;
 }
