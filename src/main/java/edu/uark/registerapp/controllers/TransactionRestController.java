a0e44f1d5c439e26920f68f3ddfb57e7246dff16
Johnny Doan <55829768+johnnydoan850@users.noreply.github.com>
2020-04-28 10:38:45 -0500

    Update TransactionRestController.java
    added delete information for the TransactionDeleteCommand

--- src/main/java/edu/uark/registerapp/controllers/TransactionRestController.java
+++ src/main/java/edu/uark/registerapp/controllers/TransactionRestController.java
@@ -6,8 +6,6 @@ import java.util.UUID;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import edu.uark.registerapp.commands.transactions.TransactionCreateCommand;
-import edu.uark.registerapp.models.api.TransactionEntry;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,12 +15,11 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import edu.uark.registerapp.commands.products.ProductCreateCommand;
-import edu.uark.registerapp.commands.products.ProductDeleteCommand;
-import edu.uark.registerapp.commands.products.ProductUpdateCommand;
+import edu.uark.registerapp.commands.transactions.TransactionCreateCommand;
+import edu.uark.registerapp.commands.transactions.TransactionDeleteCommand;
 import edu.uark.registerapp.controllers.enums.ViewNames;
 import edu.uark.registerapp.models.api.ApiResponse;
-import edu.uark.registerapp.models.api.Product;
+import edu.uark.registerapp.models.api.TransactionEntry;
 
 @RestController
 @RequestMapping(value = "/api/transaction")
@@ -50,7 +47,34 @@ public class TransactionRestController extends BaseRestController {
         .execute();
   }
 
+  @RequestMapping(value = "/{transactionId}", method = RequestMethod.DELETE)
+	public @ResponseBody ApiResponse deleteTransaction(
+		@PathVariable final UUID transactionId, 
+		final HttpServletRequest request,
+		final HttpServletResponse response
+	) {
+
+		final ApiResponse elevatedUserResponse =
+			this.redirectUserNotElevated(
+				request,
+				response,
+				ViewNames.TRANSACTION.getRoute());
+
+		if (!elevatedUserResponse.getRedirectUrl().equals(StringUtils.EMPTY)) {
+			return elevatedUserResponse;
+		}
+
+		this.transactionDeleteCommand
+			.setTransactionId(transactionId)
+			.execute();
+
+		return new ApiResponse();
+	}
+
   // Properties
   @Autowired
   private TransactionCreateCommand transactionCreateCommand;
+
+  @Autowired
+	private TransactionDeleteCommand transactionDeleteCommand;
 }
